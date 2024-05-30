4eb97ecb9022e6bc577a3fc998f85c856218b031
Ankush Rana <ankush.rana@bsc.es>
2024-05-30 15:31:56 +0200

    WIP

--- mlflow_logging.py
+++ mlflow_logging.py
@@ -1,63 +1,21 @@
 import argparse
-import sys
 from time import sleep
-import sysrsync
+# import sysrsync
 from os import environ
 import traceback
-from dotenv import load_dotenv
 import mlflow
+from dotenv import load_dotenv
 
-load_dotenv()
+destination_dir = './logs'
 
+class MlflowLogging():
+    def __init__(self, tracking_uri, experiment_name) -> None:
+        mlflow.set_tracking_uri(tracking_uri)
+        mlflow.set_experiment(experiment_name)
 
-remote_host = environ.get("REMOTE_HOST")
-remote_user = environ.get("REMOTE_USER")
-repo_name = environ.get("JOB_REPO_NAME")
-# job_name = environ.get("JOB_NAME")
-job_path = environ.get("JOB_PATH")
-mlflow_tranking_server_url = environ.get('MLFLOW_TRACKING_SERVER_URL')
-slurm_job_id = environ.get('SLURM_JOB_ID')
-exit_code = int(environ.get("ROCKET_EXIT_CODE", 0))
-
-logs_path = f"{job_path}/logs"
-destination = './logs'
-
-mlflow_run_id = environ.get("RUN_ID")
-mlflow.set_tracking_uri(mlflow_tranking_server_url)
-experiment_id = mlflow.set_experiment(repo_name).experiment_id
-
-def rsync_sync(loop=True):
-    
-    while True:
-        try:
-            sysrsync.run(
-                    source_ssh=f"{remote_user}@{remote_host}",
-                    source=logs_path,
-                    destination=destination,
-                    strict_host_key_checking=False,
-                    options=['-avh'])
-
-            mlflow.log_artifacts(destination)
-            
-        except:
-            traceback.print_exc()
-        
-
-        if not loop:
-            break
-
-        sleep(15)
-
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('task', choices=['sync', 'start', 'stop', 'artifacts-url'])
-    args = parser.parse_args()
-    
-    if args.task == 'start':
-
-        run = mlflow.start_run(run_name=slurm_job_id)
-
+    def schedule(self, run_name):
+        run = mlflow.start_run(run_name=run_name)
+        load_dotenv()
         filtered_vars = {name: value for name, value in environ.items() if name.startswith(('JOB_', 'SLURM_', 'GPFS_', 'GITHUB_'))}
 
         env_vars = dict(sorted(filtered_vars.items(), key=lambda item: (
@@ -69,28 +27,62 @@ def main():
 
         mlflow.log_params(env_vars)
         mlflow.end_run('SCHEDULED')
-        print(run.info.run_id)
+        return run.info.run_id
 
-    elif args.task == 'sync':
-        mlflow.start_run(run_id=mlflow_run_id)
-        rsync_sync()
+    def sync(self, remote_user, remote_host, source, destination):
+        try:
+            # sysrsync.run(
+            #         source_ssh=f"{remote_user}@{remote_host}",
+            #         source=source,
+            #         destination=destination,
+            #         strict_host_key_checking=False,
+            #         options=['-avh'])
 
-    elif args.task == 'artifacts-url':
-        
-        run = mlflow.get_run(run_id=mlflow_run_id)
+            mlflow.log_artifacts(destination)
+            
+        except:
+            traceback.print_exc()
+
+    
+    def syncloop(self, remote_user, remote_host, source, destination, run_id):
+        mlflow.start_run(run_id=run_id)
+        while True:
+            self._sync(remote_user, remote_host, source, destination)
+            sleep(15)
+
+    def get_artifact_url(self, run_id):
+        run = mlflow.get_run(run_id=run_id)
         print(f"{mlflow.get_tracking_uri()}/#/experiments/{run.info.experiment_id}/runs/{run.info.run_id}/artifacts")
 
-
-    elif args.task == 'stop':
-     
-        mlflow.start_run(run_id=mlflow_run_id)
-
-        rsync_sync(loop=False)
-
-        if exit_code == 0:
-            mlflow.end_run()
-        else:
+    def stop(self, remote_user, remote_host, source, destination, run_id, failed=False):
+        mlflow.start_run(run_id=run_id)
+        self.sync(remote_user, remote_host, source, destination)
+        if failed:
             mlflow.end_run('FAILED')
+        else:
+            mlflow.end_run()
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Mlflow logging')
+    parser.add_argument('-t', '--task', required=True ,help='Permited tasks: [schedule, sync, stop]')
+    parser.add_argument('--url', required=True ,help='Traking url')
+    parser.add_argument('-e', '--experiment', required=True ,help='Experiment Name')
+    parser.add_argument('-n', '--run_name', required=False ,help='Experiment Name')
+    parser.add_argument('-i', '--run_id', required=False ,help='Experiment Name')
+    parser.add_argument('-u', '--user', required=False ,help='Remote user')
+    parser.add_argument('--host', required=False ,help='Remote host')
+    parser.add_argument('-s', '--src', required=False ,help='Source files directory')
+    parser.add_argument('-f', '--failed', required=False, default=False, action='store_true' ,help='Experiment Name')
+    args = parser.parse_args()
+    
+    client = MlflowLogging(args.url, args.experiment)
+    if args.task == 'schedule':
+        run_id = client.schedule(args.run_name)
+        print(run_id)
+    if args.task == 'sync':
+        client.syncloop(args.user, args.host, args.src, destination_dir, args.run_id)
+    if args.task == 'stop':
+        client.stop(args.user, args.host, args.src, destination_dir, args.run_id)
+
+
